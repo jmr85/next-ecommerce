ac37bfa05b91f6dfa6a08d48338a85b41a07e46f
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-03-18 17:55:24 -0300

    Cambio de estilo y de logo

--- app/components/ui/Header.js
+++ app/components/ui/Header.js
@@ -3,22 +3,22 @@ import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <header className="W-full bg-gray-600">
+    <header className="W-full bg-gray-100">
         <div className="container m-auto py-6 flex justify-between items-center">
             <Image
-                src={"/ecommerce_icon.png"}
-                width={100}
-                height={100}
+                src={"/logo.png"}
+                width={200}
+                height={200}
                 alt="ecommerce icon"
             />
             <nav className="flex justify-between gap-2">
-                <a href="#" className="text-base text-slate-100 p-3">
+                <a href="#" className="text-base text-slate-800 p-3">
                     Enlace 1
                 </a>
-                <a href="#" className="text-base text-slate-100 p-3">
+                <a href="#" className="text-base text-slate-800 p-3">
                     Enlace 2
                 </a>
-                <a href="#" className="text-base text-slate-100 p-3">
+                <a href="#" className="text-base text-slate-800 p-3">
                     Enlace 3
                 </a>
             </nav>
