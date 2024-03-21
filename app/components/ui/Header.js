77e909f9264826a286b1eae18108b8ad2bd6fde3
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-03-21 18:14:27 -0300

    3.4 Navegacion-Nextjs Array de links dinamicos

--- app/components/ui/Header.js
+++ app/components/ui/Header.js
@@ -1,26 +1,46 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+
+const links = [
+    {
+        label: "Inicio",
+        href: "/"
+    },
+    {
+        label: "Nosotros",
+        href: "/nosotros"
+    },
+    {
+        label: "Contacto",
+        href: "/contacto"
+    }
+]
 
 export const Header = () => {
   return (
     <header className="W-full bg-gray-100">
         <div className="container m-auto py-6 flex justify-between items-center">
-            <Image
-                src={"/logo.png"}
-                width={200}
-                height={200}
-                alt="ecommerce icon"
-            />
+            <Link href={"/"}>
+                <Image
+                    src={"/logo.png"}
+                    width={200}
+                    height={200}
+                    alt="ecommerce icon"
+                />
+            </Link>
             <nav className="flex justify-between gap-2">
-                <a href="#" className="text-base text-slate-800 p-3">
-                    Enlace 1
-                </a>
-                <a href="#" className="text-base text-slate-800 p-3">
-                    Enlace 2
-                </a>
-                <a href="#" className="text-base text-slate-800 p-3">
-                    Enlace 3
-                </a>
+                {
+                    links.map(link => {
+                        return <Link 
+                          key={link.label}
+                          href={link.href}
+                          className={`text-base text-slate-600 p-3`}
+                        >
+                            {link.label}
+                        </Link>
+                    })
+                }
             </nav>
         </div>
     </header>
