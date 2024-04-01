617369827ee5e1fab83643614ce30422c33dace5
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-04-01 02:30:50 -0300

    4.2 Caching y Revalidate Nextjs

--- app/posts/page.js
+++ app/posts/page.js
@@ -2,7 +2,14 @@ import React from 'react'
 import Link from "next/link";
 
 const getPosts = async() => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', 
+        {
+            cache: "no-store",
+            next: {
+                revalidate: 0 //con cero no catchea nunca
+            }
+        }
+    )
 
     if(!response.ok){
         throw new Error("Falló la obtención de datos");
