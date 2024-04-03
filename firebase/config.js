ea1e3636bf96093dfed184de62cefd58725da4b7
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-04-03 03:49:18 -0300

    5. Data Fetching II - 5.3 Integrando Firebase

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_TcV7lXY9fxpZqH2FxHqjbSyKviuLpAE",
+  authDomain: "ecommerce-nextjs-04-2024.firebaseapp.com",
+  projectId: "ecommerce-nextjs-04-2024",
+  storageBucket: "ecommerce-nextjs-04-2024.appspot.com",
+  messagingSenderId: "448877737965",
+  appId: "1:448877737965:web:c1d5e1af704ad97a2459e4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
