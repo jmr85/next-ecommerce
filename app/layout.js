ba6dae2ca1109ca60c4dd3e964a0e1511cbf0094
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-04-04 13:24:04 -0300

    5.7 Context y Providers II - Importacion CartContext en RootLayout de la App

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css"
 import { Header } from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
+import { CartProvider } from "@/components/context/CartContext";
 
 // export const metadata = {
 //   title: "Create Next App",
@@ -11,9 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header/>
-        {children}
-        <Footer/>
+        <CartProvider>
+          <Header/>
+          {children}
+          <Footer/>
+        </CartProvider>
       </body>
     </html>
   );
