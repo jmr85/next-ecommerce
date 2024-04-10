cba6aeb888d4023525f47992420fa10ed19e6906
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-04-10 15:54:35 -0300

    6. Integracion - 6.5 Parallel Routes

---
+++ app/admin/layout.js
@@ -0,0 +1,17 @@
+'use client'
+import { useAuthContext } from "@/components/context/AuthContext"
+
+const AdminLayout = ({children, login}) => {
+    const {user} = useAuthContext()
+  return (
+    <>
+        {
+            user.logged
+            ? children
+            : login
+        }
+    </>
+  )
+}
+
+export default AdminLayout
