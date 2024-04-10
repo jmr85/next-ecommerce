1b99607f7a33db49d3e2152690e2d5eff980c56d
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-04-10 16:28:16 -0300

    Only comment about Parallel Routes and Slots

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,6 +1,13 @@
 'use client'
 import { useAuthContext } from "@/components/context/AuthContext"
 
+// Parallel Routes allows you to simultaneously or conditionally render one or more pages within the same layout. 
+// They are useful for highly dynamic sections of an app, such as dashboards and feeds on social sites.
+// Parallel routes are created using named slots. Slots are defined with the @folder
+// folder -> @login/page.js (slot)
+// Slots are passed as props to the shared parent layout.
+// https://nextjs.org/docs/app/building-your-application/routing/parallel-routes
+
 const AdminLayout = ({children, login}) => {
     const {user} = useAuthContext()
   return (
