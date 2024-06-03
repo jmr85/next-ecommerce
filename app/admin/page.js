0f270df4da4d9e70850615c49c7df88d460e6340
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-06-02 23:05:58 -0300

    added Link to Orders

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import ProductsTable from "@/components/admin/ProductsTable"
 import LogoutButton from "@/components/admin/LogoutButton"
 
@@ -9,7 +10,13 @@ export const metadata = {
 const Admin = () => {
   return (
     <div className="container m-auto mt-6"> 
-      <LogoutButton/> 
+      <LogoutButton/>
+      <Link
+        href={"/admin/orders"}
+        className="rounded bg-blue-600 p-2 text-white m-4"
+      >
+        Ver ordenes
+      </Link> 
       <h2 className="text-2xl my-10 border-b pb-4">
         Panel Administración
       </h2>
